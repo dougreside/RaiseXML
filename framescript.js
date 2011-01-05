fe9c92757cb44c40469dab363583e2f761643794
Doug Reside <dreside@doug-resides-macbook-2.local>
2011-01-05 13:26:52 -0500

    Some git problems.  Color should now work

--- framescript.js
+++ framescript.js
@@ -1,7 +1,33 @@
 
 		alert("po");
-			
+			var shifted =false;
 			ts = new TextSelector();
-			sel = ts.getSelectedText();
-			alert(JSON.stringify(sel));
-			window.parent.showMessage(JSON.stringify(sel));
+			
+			$("body").eq(0).keydown(function(e){
+                
+					switch (e.charCode) {
+						
+                    			
+                       
+                        case 60:
+							// <
+							window.parent.showMessage("Tag Opened");	
+							sel = ts.getSelectedText();
+							
+							kidlen=$(sel.StartParent).children().length;
+							
+							output = $("body", d).html();
+							start = output.substring(0,sel.StartOffset);
+							end = output.substring(sel.StartOffset);
+							middle = "<span class='tag'>&lt;</span>";
+							node.html(start+middle+end);
+                        break;     
+						case 62:
+							// <
+							window.parent.showMessage("Tag Closed");	
+                        break;                        
+                    }
+            
+                    
+                });	
+	
