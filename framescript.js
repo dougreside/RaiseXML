fa26d887eb6d6ae536e7be8cc1d2926bc8740c7a
Doug Reside <dreside@doug-resides-macbook-2.local>
2011-01-05 08:57:39 -0500

    A few fixes to editor

--- framescript.js
+++ framescript.js
@@ -1,55 +1,7 @@
-$(document).ready(function(){
+
 		alert("po");
-			var shifted =false;
-			ts = new TextSelector();
 			
-			$("body").mouseup(function(e){sel = ts.getSelectedText();window.parent.showMessage(JSON.stringify(sel));});
-		            $("html").eq(0).keydown(function(e){
-                
-					switch (e.keyCode) {
-						
-                        case 16:
-						shifted = true;
-							// Return 
-                          //  Pause();
-                           
-						break;
-                        case 188:
-							// <
-						
-							if (shifted) {
-						
-							sel = ts.getSelectedText();
-							window.parent.showMessage(JSON.stringify(sel));	
-							kidlen=$(sel.StartParent).children().length;
-							node = null;
-							if (kidlen>0){
-								node = $(sel.StartParent).children().eq(sel.StartChild);
-								}
-							else{
-								node =  $(sel.StartParent);
-							}
-							output = node.html();
-							start = output.substring(0,sel.StartOffset);
-							end = output.substring(sel.StartOffset);
-						
-							middle = "<span class='tag'>&lt;</span>";
-							node.html(start+middle+end);
-							
-							}
-                        break;
-                        case 190:
-							// <
-							if (shifted) {
-							
-							}
-                        break;
-                            
-                    }
-					if (e.keyCode!=16){
-						shifted = false;
-					}
-            
-                    
-                });	
-		});
+			ts = new TextSelector();
+			sel = ts.getSelectedText();
+			alert(JSON.stringify(sel));
+			window.parent.showMessage(JSON.stringify(sel));
